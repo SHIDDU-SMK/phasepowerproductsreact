09048edd8fd9cccbdae44a0d10dbdcd01a23bc0e
vani-yaraganavi <vaniy8457@gmial.com>
2023-07-21 12:16:40 +0530

    In footer screen  Changed the address.

--- src/footer.js
+++ src/footer.js
@@ -19,51 +19,53 @@ class Footer extends Component {
     return (         
         <div id="contact" style={footerbgColor}>
               <Row>
-                    <Col md={6}>
+              <Col md={6}>
 
-                      <Row>
-                        <Col md={12}>
-                          <h4 style={{color:"white"}}>
-                            <b>Contact</b>
-                          </h4>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col md={5}>
-                          <p className="card-text" style={{color:"white"}}>
-                            Phase Power Products <br/> 
-                            8/16, opp.KTG School,  <br/>
-                            40 Feet Rd,  <br/>
-                            Srigandha Nagar, Hegganahalli,  <br/>
-                            Bengaluru, Karnataka 560091 <br />
-                              GST N0-29AASFP4396C1ZO 
-                          </p>
-                        </Col>
-                        <Col md={7} style={{ color: "white" }}>
-                            <p className="card-text" >
-                              <i className="fa fa-phone" style={{paddingRight:"1rem"}}></i>
-                              +91 80 28365150 <br />
-                              +91 7353775063, +91 7090026669, <br /> 
-                              +91 7090036669, +91 7090046669
-                            </p>
-                            <p>
-                              <i class="fa fa-envelope" style={{ "font-size": "16px", color: "white" }}></i> 
-                              <Link mailto="sales@PhasePowerProducts.in" style={{ color: "white", paddingLeft:"1rem" }}>
-                                  sales@phasepowerproducts.in
-                              </Link><br/> 
-                              <i class="fa fa-envelope" style={{ "font-size": "16px", color: "white"  }}></i>
-                              <Link mailto="phasepowerproducts@gmail.com" style={{ color: "white", paddingLeft:"1rem" }}>
-                                  phasepowerproducts@gmail.com
-                              </Link>
-                            </p>
-                            <p>
-                                <a className="fa" href="https://facebook.com/" style={{color:"white", paddingRight:"1rem"}} ><i className="fa fa-facebook-square" style={footerTextSize}></i></a>
-                                <a className="fa" href="https://linkedin.com/" style={{color:"white", paddingRight:"1rem"}}><i className="fa fa-linkedin " style={footerTextSize}></i></a>
-                                <a className="fa" href="https://twitter.com" style={{color:"white"}}><i className="fa fa-twitter link-color" style={footerTextSize}></i></a>   
-                            </p>
-                        </Col>
-                      </Row>
-                    </Col>
+<Row>
+  <Col md={12}>
+    <h4 style={{color:"white"}}>
+      <b>Contact</b>
+    </h4>
+  </Col>
+</Row>
+<Row>
+  <Col md={6}>
+    <p className="card-text" style={{color:"white"}}>
+      Phase Power Products <br/> 
+      1st Floor, 15th Cross, 
+      GKW Layout  <br/>
+      Doddanna Industrial Area, <br/> 
+      Near Peenya 2nd stage,   <br/>
+      Srigandha Nagar, Hegganahalli,  <br/>
+      Bangalore-560058. <br />
+      GST N0-29AASFP4396C1ZO 
+    </p>
+  </Col>
+  <Col md={6} style={{ color: "white" }}>
+      <p className="card-text" >
+        <i className="fa fa-phone" style={{paddingRight:"0.5rem"}}></i>
+        +91 80 28365150 <br />
+        <i className="fa fa-phone" style={{paddingRight:"0.5rem"}}></i> +91 7353775063 <br /> 
+        <i className="fa fa-phone" style={{paddingRight:"0.5rem"}}></i> +91 7090026669/36669/46669
+        </p>
+      <p>
+        <i class="fa fa-envelope" style={{ "font-size": "16px", color: "white" }}></i> 
+        <Link mailto="sales@PhasePowerProducts.in" style={{ color: "white", paddingLeft:"0.25rem" }}>
+            sales@phasepowerproducts.in
+        </Link><br/> 
+        <i class="fa fa-envelope" style={{ "font-size": "16px", color: "white"  }}></i>
+        <Link mailto="phasepowerproducts@gmail.com" style={{ color: "white", paddingLeft:"0.25rem" }}>
+            phasepowerproducts@gmail.com
+        </Link>
+      </p>
+      <p>
+          <a className="fa" href="https://facebook.com/" style={{color:"white", paddingRight:"1rem"}} ><i className="fa fa-facebook-square" style={footerTextSize}></i></a>
+          <a className="fa" href="https://linkedin.com/" style={{color:"white", paddingRight:"1rem"}}><i className="fa fa-linkedin " style={footerTextSize}></i></a>
+          <a className="fa" href="https://twitter.com" style={{color:"white"}}><i className="fa fa-twitter link-color" style={footerTextSize}></i></a>   
+      </p>
+  </Col>
+</Row>
+</Col>
                     <Col lg={6}>
                           <Row>
                             <h4 className="card-title" style={{paddingLeft : '60px', color:"white"}}>
