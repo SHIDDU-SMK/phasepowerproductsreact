8fefd1ec9c830d962ff2dbacff33b432f95840c9
vani-yaraganavi <vaniy8457@gmial.com>
2023-08-23 11:57:08 +0530

    All the changes are done.

--- src/clients.js
+++ src/clients.js
@@ -1,46 +1,55 @@
-import React from 'react';
-import Image from 'react-bootstrap/Image';
-import BMRCL from './assets/images/bmrcl.webp';
-import Pgrid from './assets/images/powergrid.webp';
-import Loha from './assets/images/lohalogo.webp';
-import ESS from './assets/images/ess.webp';
-import SldLogo from './assets/images/sld.webp';
-import { Row, Col, Container } from 'react-bootstrap';
+import React from "react";
+import Image from "react-bootstrap/Image";
+import BMRCL from "./assets/images/bmrcl.webp";
+import Pgrid from "./assets/images/powergrid.webp";
+import Loha from "./assets/images/lohalogo.webp";
+import ESS from "./assets/images/ess.webp";
+import SldLogo from "./assets/images/sld.webp";
+import { Row, Col, Container } from "react-bootstrap";
+import Applo from './assets/images/apolo.png'
+import DPKU from './assets/images/DPKU.png'
 
+function Clients() {
+  return (
+    <>
+      <Container>
+        <h2 className="featurette-heading" style={{ paddingBottom: "4rem" }}>
+          Our clients
+        </h2>
+        <Row style={{ background: "rgba(248, 248, 248, 0.82)" }}>
+          <Col sm={4} className="zoom image-center">
+            <Image src={BMRCL} alt="bmrcl image" width="100%" />
+          </Col>
+          <Col sm={4} className="zoom image-center">
+            <Image src={SldLogo} alt="brand image" width="100%" />
+          </Col>
+          <Col sm={4} className="zoom image-center">
+            <Image src={Loha} alt="brand image" width="100%" />
+          </Col>
+        </Row>
+        <Row style={{ background: "rgba(248, 248, 248, 0.82)" }}>
+          <Col sm={4} className="zoom image-center">
+            <Image src={ESS} alt="brand image" width="100%" />
+          </Col>
 
- function Clients() {
-    return (
-         <>
-            <Container>
-            <h2 className="featurette-heading" style={{paddingBottom:"4rem"}}>
-                    Our clients
-                </h2>
-                <Row style={{background: "rgba(248, 248, 248, 0.82)"}}>
-                    <Col sm={4} className="zoom image-center">
-                    <Image src={BMRCL} alt="bmrcl image" width="100%"  />
-                    </Col>
-                    <Col sm={4} className="zoom image-center">
-                    <Image src={SldLogo} alt="brand image" width="100%"  /> 
-                    </Col> 
-                    <Col sm={4} className="zoom image-center">
-                    <Image src={Loha} alt="brand image" width="100%"  />
-                    </Col>
-                    
-                </Row>
-                <Row style={{background: "rgba(248, 248, 248, 0.82)"}}>
-                    <Col sm={4} className="zoom image-center">
-                    <Image src={ESS} alt="brand image" width="100%"  />
-                    </Col>
-                    
-                    
-                    <Col sm={4} className="zoom image-center">
-                    <Image src={Pgrid} alt="brand image" width="100%" />
-                    </Col>
-                </Row >
-                <hr className="featurette-divider" style={{borderColor: 'transparent'}}/>
-            </Container>
-         </>
-    )
-
- }
- export default Clients;
+          <Col sm={4} className="zoom image-center">
+            <Image src={Pgrid} alt="brand image" width="100%" />
+          </Col>
+          <Col sm={4} className="zoom image-center">
+            <Image src={Applo} alt="brand image" width="220px" height="130px" />
+          </Col>
+        </Row>
+        <Row style={{ background: "rgba(248, 248, 248, 0.82)" }}>
+          <Col sm={4} className="zoom image-center">
+            <Image src={DPKU} alt="dpk image" width="124px" height="100px" />
+          </Col>
+        </Row>
+        <hr
+          className="featurette-divider"
+          style={{ borderColor: "transparent" }}
+        />
+      </Container>
+    </>
+  );
+}
+export default Clients;
--- src/photogallary.js
+++ src/photogallary.js
@@ -8,21 +8,22 @@ import { Container } from "react-bootstrap";
 import LightBox, { Modal, ModalGateway } from "react-images";
 
 const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 4
+  },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 4,
-    slidesToSlide: 4, // optional, default to 1.
+    items: 4
   },
   tablet: {
-    breakpoint: { max: 1024, min: 768 },
-    items: 3,
-    slidesToSlide: 3, // optional, default to 1.
+    breakpoint: { max: 1024, min: 464 },
+    items: 1
   },
   mobile: {
-    breakpoint: { max: 767, min: 464 },
-    items: 2,
-    slidesToSlide: 1, // optional, default to 1.
-  },
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
 };
 
 const sliderImageUrl = [
